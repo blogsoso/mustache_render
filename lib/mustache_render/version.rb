021971985f133dc1d0aaf6c4e5f19a58e13a90df
WangJinzhong <andywang7259@gmail.com>
2013-01-12 00:18:40 +0800

    release version 0.0.4

--- lib/mustache_render/version.rb
+++ lib/mustache_render/version.rb
@@ -3,7 +3,7 @@ module MustacheRender
   VERSION_NUMBERS = [
     VERSION_MAJOR = 0,
     VERSION_MINOR = 0,
-    VERSION_BUILD = 3,
+    VERSION_BUILD = 4,
   ]
 
   VERSION = VERSION_NUMBERS.join(".")
