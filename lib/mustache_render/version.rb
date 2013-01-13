07aa1e6eacd56ee9f186e33f5e35ed72572c8e3c
WangJinzhong <andywang7259@gmail.com>
2013-01-13 19:31:25 +0800

    version edit

--- lib/mustache_render/version.rb
+++ lib/mustache_render/version.rb
@@ -3,7 +3,7 @@ module MustacheRender
   VERSION_NUMBERS = [
     VERSION_MAJOR = 0,
     VERSION_MINOR = 0,
-    VERSION_BUILD = 6,
+    VERSION_BUILD = 7,
   ]
 
   VERSION = VERSION_NUMBERS.join(".")
